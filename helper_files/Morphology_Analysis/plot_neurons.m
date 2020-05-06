bda2a6e9047982eae12ff41b0da16b67756c7dd1
Brandon <bmark@uoregon.edu>
2020-05-06 12:57:02 -0700

    Added ability to change the soma color.

--- helper_files/Morphology_Analysis/plot_neurons.m
+++ helper_files/Morphology_Analysis/plot_neurons.m
@@ -1,4 +1,4 @@
-function pg = plot_neurons(Neurons,color,alpha,dim,soma,synapses,strahler_sizing)
+function pg = plot_neurons(Neurons,color,alpha,dim,soma,synapses,strahler_sizing,soma_color)
 
 for i = 1:length(Neurons)
     
@@ -6,13 +6,28 @@ for i = 1:length(Neurons)
     hold on
     if dim == 3
        pg = plot(Neurons(i).skeleton_data.Graph,'XData',Neurons(i).Skeleton_Coords(:,1),'YData',Neurons(i).Skeleton_Coords(:,2),'ZData',Neurons(i).Skeleton_Coords(:,3),'EdgeColor',color,'NodeColor',color,'MarkerSize',.001,'LineWidth',1,'EdgeAlpha',alpha,'ShowArrows','Off');
-
+       if nargin > 6 
+           if strahler_sizing == 1
+               sn = strahler_number(Neurons(i));
+                for kk = 1:length(sn)
+                    highlight(pg,kk,successors(Neurons(i).skeleton_data.Graph,kk),'LineWidth',sn(kk));
+                    highlight(pg,kk,'MarkerSize',sn(kk));
+                end
+           else
+           end
+       else
+       end
+       
+        if exist('soma_color','var')
+        else
+            soma_color = color
+        end
         if soma == 1
             [X, Y, Z] = sphere(300);
             X = 2000*X + Neurons(i).Soma_Coords(1);
             Y= 2000*Y + Neurons(i).Soma_Coords(2);
             Z = 2000*Z + Neurons(i).Soma_Coords(3);
-            surf(X,Y,Z,'FaceColor',color,'LineStyle','none','FaceAlpha',1);
+            surf(X,Y,Z,'FaceColor',soma_color,'LineStyle','none','FaceAlpha',1);
             
         else 
         end
